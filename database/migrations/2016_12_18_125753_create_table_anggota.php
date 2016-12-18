b4bc01366ae816e6a5b609cb687e81e76e312f1b
gaspan <gaspan619@gmail.com>
2016-12-18 21:21:50 +0700

    add restfull api

---
+++ database/migrations/2016_12_18_125753_create_table_anggota.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAnggota extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anggotas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('email')->unique();
+            $table->string('password');            
+            $table->string('komunitas');
+            $table->string('kampus');
+            $table->string('alamatKampus');
+            $table->string('deskripsi');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anggota');
+    }
+}
---
+++ database/migrations/2016_12_18_134145_create_table_pengajuan.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePengajuan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengajuans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_anggota')->unsigned();
+            $table->string('proposal');
+            $table->enum('status_valid', ['belum','terima','tolak']);
+            $table->enum('status_rev', ['belum','terima','tolak']);
+            $table->timestamps();
+
+            $table->foreign('id_anggota')->references('id')->on('anggotas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengajuan');
+    }
+}
