5b0d063ffabf4efa55316c9da272c55acf3d7c81
gaspan <gaspan619@gmail.com>
2016-12-20 22:09:12 +0700

    update perusahaaan controller

---
+++ app/Http/Controllers/PerusahaanController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\perusahaan;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Transformers\PerusahaanTransformer;
+use Auth;
+
+class PerusahaanController extends Controller
+{
+    protected $rules = [
+        'nama'      => 'required',
+        'alamat'    => 'required|min:15',
+        'email'     => 'required|email',
+        'password'  => 'required|min:6',
+        'deskripsi' => 'required|min:10'
+
+    ];
+    public function showAll(perusahaan $perusahaan)
+    {
+        $perusahaans = $perusahaan->all();
+
+        $response = fractal()
+            ->collection($perusahaans)
+            ->transformWith(new PerusahaanTransformer)
+            ->toArray();
+
+        return response()->json($response, 201);
+        
+    }
+
+    public function add(Request $request, perusahaan $perusahaan)
+    {
+         if (!is_array($request->all()))
+        {
+            return ['error' => 'request harus berbentuk array'];
+        }
+        try
+        {
+            $validator = \Validator::make($request->all(), $this->rules);
+            if($validator->fails())
+            {
+                return response()->json([
+                    'created' => false,
+                    'errors'  => $validator->errors()->all()
+                ], 500);
+            }
+            else
+            {
+                $perusahaan = $perusahaan->create($request->all());
+
+                $response = fractal()
+                    ->item($perusahaan)
+                    ->transformWith(new PerusahaanTransformer)
+                    ->toArray();
+
+                return response()->json($response, 201);
+            }
+        }
+        catch (Exception $e)
+        {
+            \Log::info('Error creating data perusahaan: ' .$e);
+            return response()->json(['created' => false], 500);
+        }
+    }
+
+    
+    public function profileId($id, perusahaan $perusahaan)
+    {
+        $perusahaan = $perusahaan->findorFail($id);
+        return fractal()
+            ->item($perusahaan)
+            ->transformWith(new PerusahaanTransformer)
+            ->toArray();       
+    }
+
+    
+    public function updatePerusahaan(Request $request, $id, perusahaan $perusahaan)
+    {
+        if (!is_array($request->all()))
+        {
+            return ['error' => 'request harus berbentuk array'];
+        }
+        try
+        {
+            $validator = \Validator::make($request->all(), $this->rules);
+            if($validator->fails())
+            {
+                return response()->json([
+                    'updated' => false,
+                    'errors'  => $validator->errors()->all()
+                ], 500);
+            }
+            else
+            {
+              $perusahaan = perusahaan::find($id);
+              $perusahaan->update($request->all());
+
+                $response = fractal()
+                    ->item($perusahaan)
+                    ->transformWith(new PerusahaanTransformer)
+                    ->toArray();
+
+                return response()->json($response, 201);
+            }
+        }
+        catch (Exception $e)
+        {
+            \Log::info('Error updating data perusahaan: ' .$e);
+            return response()->json(['created' => false], 500);
+        }
+    }
+
+    
+    public function deletePerusahaan($id)
+    {
+        $perusahaan = perusahaan::findorFail($id);
+        $perusahaan->delete();
+
+        return response()->json([
+            'pesan' => 'perusahaan sudah bangkrut/mengundurkan diri entahlah'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,9 @@ Route::post('add/admin','AdminController@store');
 Route::get('admin/profile/{id}','AdminController@showById');
 Route::put('admin/update/{id}','AdminController@updateAdmin');
 Route::delete('admin/delete/{id}','AdminController@deleteAdmin');
+
+Route::get('perusahaan','PerusahaanController@showAll');
+Route::get('perusahaan/profile/{id}','PerusahaanController@profileId');
+Route::post('perusahaan/add','PerusahaanController@add');
+Route::put('perusahaan/update/{id}','PerusahaanController@updatePerusahaan');
+Route::delete('perusahaan/del/{id}','PerusahaanCOntroller@deletePerusahaan');
