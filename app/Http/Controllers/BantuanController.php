54f861bf119022acf0bd1bafe6754e5189b7adab
gaspan <gaspan619@gmail.com>
2016-12-29 17:54:42 +0700

    update join join

--- app/Http/Controllers/BantuanController.php
+++ app/Http/Controllers/BantuanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use App\bantuan;
 use Illuminate\Http\Request;
 use App\Http\Requests;
@@ -13,6 +14,9 @@ class BantuanController extends Controller
     protected $rules = [
         'id_pengajuan'      => 'required|integer',
         'jumlah_dana'       => 'required|numeric'
+    ];
+    protected $rulesupdt = [
+        'jumlah_dana'       => 'required',
     ];
 
     public function createBantuan(Request $request, bantuan $bantuan)
@@ -52,5 +56,54 @@ class BantuanController extends Controller
             return response()->json(['created' => false], 500);
         }
 
+    }
+
+    public function updateBantuan(bantuan $bantuan, Request $request, $id)
+    {
+          if (!is_array($request->all()))
+        {
+            return ['error' => 'request harus berbentuk array'];
+        }
+        try
+        {
+            $validator = \Validator::make($request->all(), $this->rulesupdt);
+            if($validator->fails())
+            {
+                return response()->json([
+                    'updated' => false,
+                    'errors'  => $validator->errors()->all()
+                ], 500);
+            }
+            else
+            {
+              $bantuan = bantuan::find($id);
+              $bantuan->update([
+                    'jumlah_dana'      => $request->jumlah_dana,
+              ]);
+
+                $response = fractal()
+                    ->item($bantuan)
+                    ->transformWith(new BantuanTransformer)
+                    ->toArray();
+
+                return response()->json($response, 201);
+            }
+        }
+        catch (Exception $e)
+        {
+            \Log::info('Error updating dana: ' .$e);
+            return response()->json(['updated' => false], 500);
+        }
+    }
+
+    public function viewBantuan(bantuan $bantuan, $id)
+    {
+        $response = DB::table('bantuans')
+                            ->join('pengajuans','bantuans.id_pengajuan','=','pengajuans.id')
+                            ->join('anggotas','pengajuans.id_anggota','=','anggotas.id')
+                            ->select('anggotas.nama','anggotas.email','anggotas.kampus','anggotas.alamatKampus','bantuans.jumlah_dana')
+                            ->where('id_anggota',$id)
+                            ->get();
+        return response()->json($response, 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -35,3 +35,5 @@ Route::get('anggota/hasil/{id}','ProposalController@viewHasilReview');
 
 Route::get('perusahaan/diterima','ProposalController@showProposalDiterima');
 Route::post('perusahaan/bantuan','BantuanController@createBantuan');
+Route::put('perusahaan/ubahbantuan/{id}','BantuanController@updateBantuan');
+Route::get('perusahaan/viewbantuan/{id}','BantuanController@viewBantuan');
